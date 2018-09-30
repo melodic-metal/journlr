766f532161b174911df677dcafdfc80c68797673
Brendan Jones <bitbucket@bacafe.xyz>
2018-09-30 19:02:52 +0800

    updated sql

--- db.sql
+++ db.sql
@@ -3,8 +3,8 @@ CREATE DATABASE journlr ;
 CREATE TABLE `journlr`.`users`
 (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(50) NOT NULL,
-    `password` VARCHAR(100) NOT NULL,
+    `email` VARCHAR(128) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
     `firstname` VARCHAR(32) NOT NULL,
     `lastname` VARCHAR(32) NOT NULL,
 
@@ -14,12 +14,11 @@ PRIMARY KEY (`id`)
 CREATE TABLE `journlr`.`journlr`
 (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(50) NOT NULL,
     `owner_id` VARCHAR(48) NOT NULL,
-    `firstName` VARCHAR(32) NOT NULL,
-    `lastName` VARCHAR(32) NOT NULL,
-    `title` VARCHAR(32) NOT NULL,
-    `postbody` TEXT(32) NOT NULL,
+    `firstName` VARCHAR(48) NOT NULL,
+    `lastName` VARCHAR(48) NOT NULL,
+    `title` VARCHAR(255) NOT NULL,
+    `postbody` TEXT NOT NULL,
     `created_at` TIMESTAMP NOT NULL,
 PRIMARY KEY (`id`)
 );
